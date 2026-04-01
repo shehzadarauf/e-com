a5168799c0998d79e328c539887134603f45a1c7
Abdul Rauf <raufie384@gmail.com>
2026-04-01 22:42:39 +0500

    fix: correct import path for DashboardView component

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/auth/loginView.vue";
 import SignupView from "@/views/auth/signupView.vue";
-import DashboardView from "@/views/dashboard/DashboardView.vue";
+import DashboardView from "@/views/dashboard/dashboardView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
